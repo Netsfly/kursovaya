576e5e6737a949f4f936e4d1a88ae2b95a07e7f7
Netsfly <sasha.tsoi20@gmail.com>
2025-02-23 17:28:26 +0600

    repair

--- tic_tac_toe/tictak.py
+++ tic_tac_toe/tictak.py
@@ -2,6 +2,7 @@ import pygame as p
 
 p.init()
 
+
 class Square(p.sprite.Sprite):
     def __init__(self, x_id, y_id, number):
         super().__init__()
@@ -9,37 +10,41 @@ class Square(p.sprite.Sprite):
         self.height = 120
         self.x = x_id * self.width
         self.y = y_id * self.height
-        self.content = ''
+        self.content = ' '
         self.number = number
         self.image = blank_image
-        self.image = p.transform.scale(self.image,(self.width, self.height))
+        self.image = p.transform.scale(self.image, (self.width, self.height))
         self.rect = self.image.get_rect()
-        
+
     def update(self):
         self.rect.center = (self.x, self.y)
-        
+
     def clicked(self, x_val, y_val):
         global turn
-        if self.content == '  ':
+
+        if self.content == ' ':
             if self.rect.collidepoint(x_val, y_val):
                 self.content = turn
                 board[self.number] = turn
-                
+
                 if turn == 'x':
                     self.image = x_image
                     self.image = p.transform.scale(self.image, (self.width, self.height))
                     turn = 'o'
+
                 else:
                     self.image = o_image
                     self.image = p.transform.scale(self.image, (self.width, self.height))
                     turn = 'x'
-        
+
+
 def Update():
     win.blit(background, (0, 0))
     square_group.draw(win)
     square_group.update()
     p.display.update()
-    
+
+
 WIDTH = 500
 HEIGHT = 500
 
@@ -47,26 +52,25 @@ win = p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption('Tic Tac Toe')
 clock = p.time.Clock()
 
-blank_image = p.image.load(r"d:\tic tac\tic_tac_toe\Blank.png")
-x_image = p.image.load(r"d:\tic tac\tic_tac_toe\x.png")
-o_image = p.image.load(r"d:\tic tac\tic_tac_toe\o.png")
-background_image = p.image.load(r"d:\tic tac\tic_tac_toe\Background.png")
-background = p.transform.scale(background_image, (WIDTH, HEIGHT)) 
+blank_image = p.image.load('Blank.png')
+x_image = p.image.load('x.png')
+o_image = p.image.load('o.png')
+background = p.image.load('Background.png')
+
+background = p.transform.scale(background, (WIDTH, HEIGHT))
 
 square_group = p.sprite.Group()
 squares = []
 board = [' ' for i in range(10)]
 
-
 num = 1
-for y in range(1 ,4):
+for y in range(1, 4):
     for x in range(1, 4):
         sq = Square(x, y, num)
         square_group.add(sq)
         squares.append(sq)
-        
-        num += 1
 
+        num += 1
 
 turn = 'x'
 run = True
@@ -75,10 +79,10 @@ while run:
     for event in p.event.get():
         if event.type == p.QUIT:
             run = False
-    
-        if event.type ==  p.MOUSEBUTTONDOWN and turn == 'x':
+
+        if event.type == p.MOUSEBUTTONDOWN and turn == 'x':
             mx, my = p.mouse.get_pos()
             for s in squares:
                 s.clicked(mx, my)
-    
+
     Update()
