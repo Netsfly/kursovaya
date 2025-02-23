be5ebd370215700030145f875a712565cc59e898
Netsfly <sasha.tsoi20@gmail.com>
2025-02-23 16:37:56 +0600

    working background

--- tic_tac_toe/tictak.py
+++ tic_tac_toe/tictak.py
@@ -6,15 +6,55 @@ class Square(p.sprite.Sprite):
     def __init__(self, x_id, y_id, number):
         super().__init__()
         self.width = 120
-        self.heght = 120
+        self.height = 120
         self.x = x_id * self.width
-        self.y = y_id * self.heght
+        self.y = y_id * self.height
         self.content = ''
         self.number = number
         self.image = blank_image
-        self.image = p.transform.scale(self.image,(self.width, self.heght))
+        self.image = p.transform.scale(self.image,(self.width, self.height))
+        self.rect = self.image.get_rect()
         
-blank_image = p.image.load('Blanc.png')
-x_image = p.image.load('x.png')
-y_image = p.image.load('o.png')
-background_image = p.image.load('Background.png')
+    def update(self):
+        self.rect.center = (self.x, self.y)
+        
+def Update():
+    win.blit(background, (0, 0))
+    square_group.draw(win)
+    square_group.update()
+    p.display.update()
+    
+WIDTH = 500
+HEIGHT = 500
+
+win = p.display.set_mode((WIDTH, HEIGHT))
+p.display.set_caption('Tic Tac Toe')
+clock = p.time.Clock()
+
+blank_image = p.image.load(r"d:\tic tac\tic_tac_toe\Blank.png")
+x_image = p.image.load(r"d:\tic tac\tic_tac_toe\x.png")
+o_image = p.image.load(r"d:\tic tac\tic_tac_toe\o.png")
+background_image = p.image.load(r"d:\tic tac\tic_tac_toe\Background.png")
+background = p.transform.scale(background_image, (WIDTH, HEIGHT)) 
+
+square_group = p.sprite.Group()
+squares = []
+
+num = 1
+for y in range(1 ,4):
+    for x in range(1, 4):
+        sq = Square(x, y, num)
+        square_group.add(sq)
+        squares.append(sq)
+        
+        num += 1
+
+
+run = True
+while run:
+    clock.tick(60)
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            run = False
+    
+    Update()
